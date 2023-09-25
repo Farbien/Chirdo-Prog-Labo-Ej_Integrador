742321aa59950a163de42ff1122b0f35f9a50f1f
Nicolas Andres Chirdo <98624392+Farbien@users.noreply.github.com>
2023-09-25 09:04:24 -0300

    Add files via upload

---
+++ Entidades/Numeracion.cs
@@ -0,0 +1,126 @@
+﻿using Entidades.Enums;
+
+namespace Entidades
+{
+    public class Numeracion
+    {
+        private ESistema sistema;
+        private double valorNumerico;
+
+        public ESistema Sistema { get { return sistema; } }
+        public string Valor { get { return valorNumerico.ToString(); } }
+
+        public Numeracion(double valor, ESistema sistema)
+        {
+            this.InicializarValores(valor.ToString(), sistema);
+        }
+
+        public Numeracion(string valor, ESistema sistema)
+        {
+            this.InicializarValores(valor, sistema);
+        }
+
+        private void InicializarValores(string valor, ESistema sistema)
+        {
+            this.sistema = sistema;
+
+            if (EsBinario(valor))
+            {
+                this.valorNumerico = BinarioADecimal(valor);
+                return;
+            }
+
+            if (double.TryParse(valor, out double auxValor))
+            {
+                this.valorNumerico = auxValor;
+                return;
+            }
+            
+            this.valorNumerico = double.MinValue;
+        }
+
+        public string ConvertirA(ESistema sistema)
+        {
+            switch (sistema)
+            {
+                case ESistema.Decimal:
+                    return DecimalABinario(this.valorNumerico.ToString());
+                case ESistema.Binario:
+                    return BinarioADecimal(this.valorNumerico.ToString()).ToString();
+                default:
+                    return this.Valor;
+            }
+        }
+
+        private static bool EsBinario (string valor)
+        {
+            foreach(char c in valor)
+            {
+                if (c == '1' || c == '0')
+                    continue;
+                else
+                    return false;
+            }
+
+            return true;
+        }
+
+        private double BinarioADecimal (string valor)
+        {
+            if (!EsBinario(valor))
+                return 0;
+
+            return double.Parse(Convert.ToInt32(this.valorNumerico.ToString(), 2).ToString());
+        }
+
+        private string DecimalABinario (int valor)
+        {
+            return Convert.ToString(valor, 2);
+        }
+
+        private string DecimalABinario (string valor)
+        {
+            return this.DecimalABinario(int.Parse(valor));
+        }
+
+        public static bool operator !=(ESistema sistema, Numeracion numeracion)
+        {
+            return sistema != numeracion.sistema;
+        }
+
+        public static bool operator ==(ESistema sistema, Numeracion numeracion)
+        {
+            return sistema == numeracion.sistema;
+        }
+
+        public static Numeracion operator +(Numeracion n1, Numeracion n2)
+        {
+            return new Numeracion(n1.valorNumerico + n2.valorNumerico, ESistema.Decimal);
+        }
+
+        public static Numeracion operator -(Numeracion n1, Numeracion n2)
+        {
+            return new Numeracion(n1.valorNumerico - n2.valorNumerico, ESistema.Decimal);
+        }
+
+        public static Numeracion operator *(Numeracion n1, Numeracion n2)
+        {
+            return new Numeracion(n1.valorNumerico * n2.valorNumerico, ESistema.Decimal);
+        }
+
+        public static Numeracion operator /(Numeracion n1, Numeracion n2)
+        {
+            return new Numeracion(n1.valorNumerico / n2.valorNumerico, ESistema.Decimal);
+        }
+
+        public static bool operator !=(Numeracion n1, Numeracion n2)
+        {
+            return n1.sistema != n2.sistema;
+        }
+
+        public static bool operator ==(Numeracion n1, Numeracion n2)
+        {
+            return n1.sistema == n2.sistema;
+        }
+    }
+}
---
+++ Entidades/Operacion.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Operacion
+    {
+        private Numeracion primerOperando;
+        private Numeracion segundoOperando;
+
+        public Numeracion PrimerOperando { get { return primerOperando; } set { primerOperando = value; } }
+        public Numeracion SegundoOperando { get { return segundoOperando; } set {  segundoOperando = value; } }
+
+        public Operacion (Numeracion primerOperando, Numeracion segundoOperando) 
+        {
+            this.primerOperando = primerOperando;
+            this.segundoOperando = segundoOperando;
+        }
+
+        public Numeracion Operar (char operador)
+        {
+            switch (operador)
+            {
+                case '-':
+                    return primerOperando - segundoOperando;
+                case '*':
+                    return primerOperando * segundoOperando;
+                case '/':
+                    return primerOperando / segundoOperando;
+                default:
+                    return primerOperando + segundoOperando;
+            }
+        }
+    }
+}
