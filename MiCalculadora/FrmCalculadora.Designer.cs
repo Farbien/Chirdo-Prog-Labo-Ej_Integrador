8039f50ba0dba9b75a0e44af51246979b3ac9ff1
Nicolas Andres Chirdo <98624392+Farbien@users.noreply.github.com>
2023-09-27 21:57:27 -0300

    Add files via upload

--- MiCalculadora/FrmCalculadora.Designer.cs
+++ MiCalculadora/FrmCalculadora.Designer.cs
@@ -113,6 +113,7 @@
             // 
             // cmbOperacion
             // 
+            cmbOperacion.DropDownStyle = ComboBoxStyle.DropDownList;
             cmbOperacion.FormattingEnabled = true;
             cmbOperacion.Items.AddRange(new object[] { "+", "-", "/", "*" });
             cmbOperacion.Location = new Point(297, 250);
